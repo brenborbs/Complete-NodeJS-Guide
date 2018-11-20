68d6828c5a5a52143c2c39c020d3e6a203db9afc
brenborbs <borbonbrennon@gmail.com>
2018-11-20 11:31:58 +0800

    2nd commit completed adding ejs

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Complete NODEJS guide from Udemy
+
+2nd commit: Completed Chapter 6 and added ejs as main templating engine for rest of the course.
--- app.js
+++ app.js
@@ -5,14 +5,19 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+// template engines set-up:
+// tell express to use pug for template, then find dir
+app.set( 'view engine', 'ejs' );
+app.set('views' , 'views');
+
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public' )))
 
-app.use( '/admin', adminRoutes);
+app.use( '/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
